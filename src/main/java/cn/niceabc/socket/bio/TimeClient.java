e68e7fada6f15e3b343b083dacd9aec73589047f
马世光 <shiguang.ma@nd-english.com>
2018-11-13 14:30:01 +0800

    bio，同步阻塞

--- src/main/java/cn/niceabc/socket/bio/TimeClient.java
+++ src/main/java/cn/niceabc/socket/bio/TimeClient.java
@@ -1,4 +1,31 @@
 package cn.niceabc.socket.bio;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        try (
+                Socket socket = new Socket("127.0.0.1", port);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+        ) {
+
+            out.println("query time order");
+            String response = in.readLine();
+            System.out.println("response: "+response);
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/cn/niceabc/socket/bio/TimeServer.java
+++ src/main/java/cn/niceabc/socket/bio/TimeServer.java
@@ -1,4 +1,64 @@
 package cn.niceabc.socket.bio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TimeServer {
+    private static Logger log = LoggerFactory.getLogger(TimeServer.class);
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        ServerSocket server = new ServerSocket(port);
+
+        while (true) {
+            log.debug("server is start on port: {}", port);
+            Socket socket = server.accept();
+            new Thread(new TimeServerHandler(socket)).start();
+        }
+
+    }
+
+    private static class TimeServerHandler implements Runnable {
+        private Socket socket;
+
+        public TimeServerHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+                while (true) {
+
+                    String body = in.readLine();
+                    if (body == null) break;
+                    if ("query time order".equalsIgnoreCase(body)) {
+                        out.println("20181113");
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                    log.debug("socket is closed.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    socket = null;
+                }
+            }
+        }
+    }
 }
