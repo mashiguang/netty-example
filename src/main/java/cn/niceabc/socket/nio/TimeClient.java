e90d143e73a0b5e44ddb4776f62bec05ff63b969
mashiguang <mashiguang0833@sina.com>
2018-11-22 12:02:04 +0800

    nio

---
+++ src/main/java/cn/niceabc/socket/nio/TimeClient.java
@@ -0,0 +1,152 @@
+package cn.niceabc.socket.nio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 同步非阻塞io
+ * 使用selector多路复用器
+ * */
+public class TimeClient {
+    private static Logger log = LoggerFactory.getLogger(TimeClient.class);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        TimeClientHandler client = new TimeClientHandler();
+        new Thread(client).start();
+
+        Thread.sleep(1000);
+
+        client.sendMsg("Tom");
+    }
+
+    private static class TimeClientHandler implements Runnable {
+
+        private Selector selector;
+        private SocketChannel socketChannel;
+
+        private volatile boolean closed = false;
+
+        public TimeClientHandler() {
+
+            try {
+                selector = Selector.open();
+                socketChannel = SocketChannel.open();
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector, SelectionKey.OP_CONNECT);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        private void doConnect() throws IOException {
+            if (socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080)));
+            else socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+
+        public void sendMsg(String msg) throws IOException {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel, msg);
+        }
+
+        public void close() {
+            closed = true;
+        }
+
+        private void doWrite(SocketChannel sc, String msg) throws IOException {
+
+            ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+            sc.write(buffer);
+
+        }
+
+        private void handleInput(SelectionKey key) throws IOException {
+
+            if (!key.isValid()) return;
+
+            SocketChannel sc = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                if (sc.finishConnect());
+                else System.exit(1);
+            }
+
+            if (key.isReadable()) {
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(buffer);
+                if (readBytes < 0) {
+                    key.cancel();
+                    sc.close();
+                }
+
+                buffer.flip();
+                byte[] bytes = new byte[buffer.remaining()];
+                buffer.get(bytes);
+                String msg = new String(bytes);
+                log.debug("received: {}", msg);
+
+                closed = true;
+            }
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                doConnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+            while (!closed) {
+                try {
+                    selector.select(1000);
+                    Set<SelectionKey> keys = selector.selectedKeys();
+
+                    Iterator<SelectionKey> it = keys.iterator();
+
+                    while (it.hasNext()) {
+                        SelectionKey key = it.next();
+                        it.remove();
+
+                        try {
+                            handleInput(key);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            if (key != null) {
+                                key.cancel();
+                                if (key.channel() != null) {
+                                    key.channel().close();
+                                }
+                            }
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+
+            if (socketChannel != null) {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
---
+++ src/main/java/cn/niceabc/socket/nio/TimeServer.java
@@ -0,0 +1,136 @@
+package cn.niceabc.socket.nio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 同步非阻塞io
+ * 使用selector多路复用器
+ * */
+public class TimeServer {
+    private static Logger log = LoggerFactory.getLogger(TimeServer.class);
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        log.debug("server is start on port: {}", port);
+        new Thread(new TimeServerHandler(port)).start();
+
+    }
+
+    private static class TimeServerHandler implements Runnable {
+
+        private Selector selector;
+        private ServerSocketChannel serverSocketChannel;
+        private volatile boolean closed = false;
+
+
+        public TimeServerHandler(int port) {
+
+            try {
+                selector = Selector.open();
+                serverSocketChannel = ServerSocketChannel.open();
+                serverSocketChannel.configureBlocking(false);
+                serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+                serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        private void handleInput(SelectionKey key) throws IOException {
+
+            if (!key.isValid()) return;
+
+            if (key.isAcceptable()) {
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+
+            if (key.isReadable()) {
+                SocketChannel sc = (SocketChannel) key.channel();
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(buffer);
+                if (readBytes < 0) {
+                    key.cancel();
+                    sc.close();
+                    return;
+                }
+
+                buffer.flip();
+                byte[] bytes = new byte[buffer.remaining()];
+                buffer.get(bytes);
+                String msg = new String(bytes);
+                log.debug("received: {}", msg);
+
+                String res = "hello " + msg;
+                doWrite(sc, res);
+
+            }
+        }
+
+        private void doWrite(SocketChannel sc, String msg) throws IOException {
+
+            ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+            sc.write(buffer);
+
+        }
+
+        @Override
+        public void run() {
+
+            while (!closed) {
+                try {
+                    selector.select(1000);
+                    Set<SelectionKey> keys = selector.selectedKeys();
+
+                    Iterator<SelectionKey> it = keys.iterator();
+
+                    while (it.hasNext()) {
+                        SelectionKey key = it.next();
+                        it.remove();
+
+                        try {
+                            handleInput(key);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            if (key != null) {
+                                key.cancel();
+                                if (key.channel() != null) {
+                                    key.channel().close();
+                                }
+                            }
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (selector != null) {
+                try {
+                    selector.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
